2b75a66a5b389efdf3cefdfc11f274457f0ecb68
wdpless <wdpless@gmail.com>
2018-04-12 19:24:46 +0000

    12APR18-1
    Rebuilt module to focus on report functionality.

--- PlessMON-Agent.psm1
+++ PlessMON-Agent.psm1
@@ -34,7 +34,7 @@ function Install-PlessMON_PMA
 }
 #---------------------------------------------------------------------------------------------------------------------------------------------
 #Description: Setup Variables for PlessMON Module and functions
-#Version: 4; 8APR18
+#Version: 5; 12APR18
 #Permissions: User
 function Initialize-PlessMON_PMA
 {
@@ -44,6 +44,7 @@ function Initialize-PlessMON_PMA
     $Global:Date = get-date -format hhmm-ddMMMyy
     $Global:Today = get-date -format d
     $Global:Date2 = get-date -format ddMMMyy
+    $Timezone = get-timezone
     $Global:Titles = @($subject.PSObject.Properties.Name)
     $Global:DIR_Root = "c:\PlessMON_Agent"
     $Global:DIR_Scripts = "$DIR_Root\Scripts"
@@ -55,6 +56,10 @@ function Initialize-PlessMON_PMA
     $Global:FIL_Hardware_New = "$Dir_Reports\$Hostname-Hardware-$Date2.csv"
     $Global:FIL_Software_Baseline = "$Dir_Reports\$Hostname-Software-Baseline.csv"
     $Global:FIL_Software_New = "$Dir_Reports\$Hostname-Software-$Date2.csv"
+    $Global:Fil_Base_HardReport = "$Global:Dir_Reports\$Hostname-Hardware-Baseline.html"
+    $Global:Fil_HardReport = "$Global:Dir_Reports\$Hostname-Hardware-$Date2.html"
+    $Global:Fil_Base_SoftReport = "$Global:Dir_Reports\$Hostname-Software-Baseline.html"
+    $Global:Fil_SoftReport = "$Global:Dir_Reports\$Hostname-Software-$Date2.html"
     If(test-path "$Dir_Log\WIN_Security_Log_PM.txt")
     {} 
     Else 
@@ -80,13 +85,15 @@ $arr | Export-Csv $FIL_SysInfo -NoTypeInformation
 #/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #//////////////////////////////////////////Baseline Creation Section//////////////////////////////////////////////////////////////////////////
 #/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#----------------------------------------------------------------------------------------------------------------------------------------------
-#Description: Get Hardware information
-#Version: 4; 8APR18
+#---------------------------------------------------------------------------------------------------------------------------------------------
+#Description: Get System Hardware related information and build report
+#Version: 1; Added 12APR18
 #Permissions: Administrator
-function Get-HardwareInfo_PMA
+function Get-HardReport_PMA
 {
     Initialize-PlessMON_PMA
+    #--------------------------------------------
+    #Build csv file to collect on PlessMON-Server for data analytics
     If (Test-Path $FIL_Hardware_New)
     {
         $Global:arr = Import-CSV $FIL_Hardware_New; $Global:FIL_SysInfo = $FIL_Hardware_New
@@ -125,25 +132,52 @@ function Get-HardwareInfo_PMA
     $title = "Disk"
     Push-CSV_PM
     write-host "$title information has been added to $FIL_SysInfo"
-
+    #--------------------------------------------
+    #Build html report for hardware information
+    $ReportTitle = "$Hostname System-Hardware-Report"
+    $CSSlink = "$Global:Dir_Reports\table.css"
+    If(test-path "$Fil_Base_HardReport")
+        {
+            $HTML_Report = $FIL_HardReport
+            $when = "Updated on:"
+            If(test-path $Fil_HardReport)
+            {
+                Remove-Item $Fil_HardReport
+            }
+        }
+    Else
+        {
+            $HTML_Report = $Fil_Base_HardReport
+            $when = "Created on:"
+        }
+    $Processor = get-wmiobject win32_Processor | select Name,Caption,DeviceID,NumberOfCores,NumberOfEnabledCore,NumberOfLogicalProcessors,ThreadCount,L2CacheSize,L3CacheSize,ProcessorId,VirtualizationFirmwareEnabled,SocketDesignation,Revision,Manufacturer | convertto-html -Title $ReportTitle  -Body "<h1>$ReportTitle</h1>`n<h5>$when $(Date) $Timezone </h5>`n<h2>Processor</h2>"
+    $Memory = get-wmiobject win32_physicalmemory | select PSComputerName,BankLabel,Capacity,Caption,ConfiguredClockSpeed,DeviceLocator,FormFactor | convertto-html -Body "<h2>Memory</h2>"
+    $Memory_SN = get-wmiobject -class win32_physicalmemory | select manufacturer,serialnumber | convertto-html -Body "<h2>RAM Chip Serial Numbers</h2>"
+    $PhysicalDrive = get-physicaldisk | select FriendlyName,SerialNumber,MediaType,Size | convertto-html -Body "<h2>Physical Drive Info</h2>"
     $Users = get-ciminstance win32_useraccount | sort status | format-table -property name,description,disabled,accounttype,pscomputername
-    $subject = $Users
-    $title = "User"
-    Push-CSV_PM
-    write-host "$title information has been added to $FIL_SysInfo"
-} 
-#----------------------------------------------------------------------------------------------------------------------------------------------
-#Description: Get Hardware information
-#Version: 4; 8APR18
-#Permissions: Administrator
-function Get-SoftwareInfo_PMA
+
+    Add-Content $HTML_Report $Processor
+    Add-Content $HTML_Report $Memory_SN
+    Add-Content $HTML_Report $Memory
+    add-Content $HTML_Report $PhysicalDrive
+    Add-Content $HTML_Report $PhysicalDriveSN
+
+    write-host "Your System-Hardware-Report has been built and is located at $HTML_Report"
+}
+#---------------------------------------------------------------------------------------------------------------------------------------------
+#Description: Get System Software related information and build report
+#Version: 1; Added 12APR18
+#Permissions: User
+function Get-SoftReport_PMA
 {
     Initialize-PlessMON_PMA
+    #--------------------------------------------
+    #Build csv file to collect on PlessMON-Server for data analytics
     If (Test-Path $FIL_Software_New)
     {
         $Global:arr = Import-CSV $FIL_Software_New; $Global:FIL_SysInfo = $FIL_Software_New
     }
-Else
+    Else
     {
         If(test-path $FIL_Software_Baseline)
         {
@@ -154,80 +188,87 @@ Else
             $Global:arr = Import-CSV $FIL_SysInfo_Template; $Global:FIL_SysInfo = $FIL_Software_Baseline
         }
     }
-    Initialize-PlessMON_PMA
+    #Operating System Information
     $Global:OperatingSystem = get-wmiobject win32_OperatingSystem | select Caption,OSArchitecture,BuildNumber,Codeset,ServicePackMajorVersion,ServicePackMinorVersion,OSLanguage,OSType,InstallDate,CountryCode,EncryptionLevel,RegisteredUser,SerialNumber
     $Global:subject = $OperatingSystem
     $title = "OS"
     Push-CSV_PM
-    write-host "$title information has been added to $FIL_SysInfo"
-
+    #bios information
     $BIOS = get-wmiobject win32_bios | select Version,SMBIOSBIOSVersion,SMBIOSMajorVersion,SMBIOSMinorVersion,SerialNumber,Name,ReleaseDate,CurrentLanguage
     $subject = $BIOS
     $title = "BIOS"
     Push-CSV_PM
-    write-host "$title information has been added to $FIL_SysInfo"
-
+    #powershell version
     $PowershellVersion_3 = $PSVersionTable.PSVersion
     $subject = $PowershellVersion_3
     $title = "PS-Ver3"
     Push-CSV_PM
-    write-host "$title information has been added to $FIL_SysInfo"
-
+    #installed applications
     $Applications = wmic product get name,version
     $subject = $Applications
     $title = "Applications"
     Push-CSV_PM
-    write-host "$title information has been added to $FIL_SysInfo"
+    write-host "System Software information has been added to $FIL_SysInfo"
 
-    $Users = get-ciminstance win32_useraccount | sort status | format-table -property name,description,disabled,accounttype,pscomputername
-    $subject = $Users
-    $title = "User"
-    Push-CSV_PM
-    write-host "$title information has been added to $FIL_SysInfo"
-} 
-#---------------------------------------------------------------------------------------------------------------------------------------------
-#Description: Get System information and build report
-#Version: 4; Added 8APR18
-#Permissions: User
-function Get-SystemReport_PMA
-{
-    Initialize-PlessMON_PMA   
-    $Fil_SysInfoReportHTML = "$Global:Dir_Reports\Baseline_@$Hostname#$Date.html"
-    $filename_xml = "$Global:Dir_Reports\Baseline_@$Hostname#$Date.xml"
-    $HTMLBaseline = "$Fil_SysInfoReportHTML"
-    $XMLBaseline = "$filename_xml"
-    $ReportTitle = "$Hostname System-Information"
-    $CSSlink = "$Global:Dir_Reports\table.css"
+    #--------------------------------------------
+    #Build html report for hardware information
+    $ReportTitle = "$Hostname System-Software-Report"
+    If(test-path "$Fil_Base_SoftReport")
+    {
+        $HTML_Report = $FIL_SoftReport
+        $when = "Updated on:"
+        If(test-path $Fil_SoftReport)
+        {
+            Remove-Item $Fil_SoftReport
+        }
+    }
+    Else
+    {
+        $HTML_Report = $Fil_Base_SoftReport
+        $when = "Created on:"
+    }
     $Global:OperatingSystem = get-wmiobject win32_OperatingSystem | select Caption,OSArchitecture,BuildNumber,Codeset,ServicePackMajorVersion,ServicePackMinorVersion,OSLanguage,OSType,InstallDate,CountryCode,EncryptionLevel,RegisteredUser,SerialNumber
-    $Global:OperatingSystem2 = $Global:OperatingSystem | convertto-html -CssUri $CSSlink -Title $ReportTitle  -Body "<h1>$ReportTitle</h1>`n<h5>Updated: on $(Date)</h5>`n<h2>Operating System</h2>"
+    $Global:OperatingSystem2 = $Global:OperatingSystem | convertto-html -Title $ReportTitle  -Body "<h1>$ReportTitle</h1>`n<h5>$when $(Date) $Timezone </h5>`n<h2>Operating System</h2>"
     $BIOS = get-wmiobject win32_bios | select Version,SMBIOSBIOSVersion,SMBIOSMajorVersion,SMBIOSMinorVersion,SerialNumber,Name,ReleaseDate,CurrentLanguage | convertto-html -Body "<h2>BIOS</h2>"
     $PowershellVersion_3 = $PSVersionTable.PSVersion | convertto-html -Body "<h2>Powershell_v3</h2>"
-    $Processor = get-wmiobject win32_Processor | select Name,Caption,DeviceID,NumberOfCores,NumberOfEnabledCore,NumberOfLogicalProcessors,ThreadCount,L2CacheSize,L3CacheSize,ProcessorId,VirtualizationFirmwareEnabled,SocketDesignation,Revision,Manufacturer | convertto-html -Body "<h2>Processor</h2>"
-    $Memory = get-wmiobject win32_physicalmemory | select PSComputerName,BankLabel,Capacity,Caption,ConfiguredClockSpeed,DeviceLocator,FormFactor | convertto-html -Body "<h2>Memory</h2>"
-    $Memory_SN = get-wmiobject -class win32_physicalmemory | select manufacturer,serialnumber | convertto-html -Body "<h2>RAM Chip Serial Numbers</h2>"
-    $PhysicalDrive = get-physicaldisk | select FriendlyName,SerialNumber,MediaType,Size | convertto-html -Body "<h2>Physical Drive Info</h2>"
-    $Users = get-ciminstance win32_useraccount | sort status | format-table -property name,description,disabled,accounttype,pscomputername
-    Add-Content $HTMLBaseline $OperatingSystem2
-    Add-Content $HTMLBaseline $BIOS
-    Add-Content $HTMLBaseline $Processor
-    Add-Content $HTMLBaseline $Memory_SN
-    Add-Content $HTMLBaseline $Memory
-    add-Content $HTMLBaseline $PhysicalDrive
-    Add-Content $HTMLBaseline $PhysicalDriveSN
-    Add-Content $HTMLBaseline $PowershellVersion_3
-    write-host "Your System-info report has been built and is located at $Fil_SysInfoReportHTML"
+    $Software = Get-WmiObject Win32_Product | Sort-Object Name | select Name, Version, Vendor | convertto-html -Body "<h2>Software Applications</h2>"
+    Add-Content $HTML_Report $OperatingSystem2
+    Add-Content $HTML_Report $BIOS
+    Add-Content $HTML_Report $PowershellVersion_3
+    Add-Content $HTML_Report $Software
+    write-host "Your System-Software-Report has been built and is located at $HTML_Report"
 }
 #---------------------------------------------------------------------------------------------------------------------------------------------
-#Description: Get Logons (win event 4624 starting at 0001 this morning)
-#Version: 1
-#Permissions: Administrator
-function Get-Logon_PMA
+#Description: Get System Process information and build report
+#Version: 4; Added 8APR18
+#Permissions: User
+function Get-ProcReport_PMA
 {
-    Initialize-PlessMON_PMA
-    $lst_log_index = get-content -path "$Fil_WIN_SEC_Log_PM"
-    $event = Get-Eventlog -LogName Security -InstanceId 4624 -After $Today
-    foreach($e in $event)
-        {if ( $lst_log_index -contains $e.index){} else{$e | Format-Table -wrap >> $Fil_WIN_SEC_Log_PM}}
-    Write-host "All win event logs '4624' starting from 0001 this morning have been written to '$Fil_WIN_SEC_Log_PM'"
+    Write-host "This funtion is currently under construction."
+}
+#---------------------------------------------------------------------------------------------------------------------------------------------
+#Description: Get System service information and build report
+#Version: 4; Added 8APR18
+#Permissions: User
+function Get-ServReport_PMA
+{
+    Write-host "This funtion is currently under construction."
+}
+#---------------------------------------------------------------------------------------------------------------------------------------------
+#Description: Get System network connection information and build report
+#Version: 4; Added 8APR18
+#Permissions: User
+function Get-ConnReport_PMA
+{
+    Write-host "This funtion is currently under construction."
+}
+#----------------------------------------------------------------------------------------------------------------------------------------------
+#Description: Build Hardware,Software,Service,Process and Connection reports
+#Version: 1; Added 12APR18
+#Permissions: Administrator
+function Build-Reports_PMA
+{
+    Get-HardReport_PMA
+    Get-SoftReport_PMA
 }
 #---------------------------------------------------------------------------------------------------------------------------------------------
