03ec9897db5bb14905f4afec89a1d8d24ceb5e68
wdpless <wdpless@gmail.com>
2018-04-15 17:47:36 +0000

    gpo script

---
+++ PlessMON_GPO_Script.ps1
@@ -0,0 +1,5 @@
+﻿import-module \\192.168.5.230\Pless_Data\490_Tech_Projects\440_Scripts\Pless_MON\PlessMON-Agent.psm1
+Get-Reports_PMA
+$hostname = hostname
+New-Item -ItemType Directory \\192.168.5.230\Pless_Data\490_Tech_Projects\440_Scripts\Pless_MON\$hostname
+Move-Item -Path C:\PlessMON_Agent\Reports\*.html -Destination \\192.168.5.230\Pless_Data\490_Tech_Projects\440_Scripts\Pless_MON\$hostname\
