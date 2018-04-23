692625a6d0d873b710e3b748ab6de6ba65d1e66c
wdpless <wdpless@gmail.com>
2018-04-23 18:23:31 +0000

    23APR18 tested operation of GPO script
    23APR18 tested operation of GPO script

--- PlessMON_GPO_Script.ps1
+++ PlessMON_GPO_Script.ps1
@@ -1,5 +1,29 @@
 ﻿import-module \\192.168.5.230\Pless_Data\490_Tech_Projects\440_Scripts\Pless_MON\PlessMON-Agent.psm1
 Get-Reports_PMA
 $hostname = hostname
-New-Item -ItemType Directory \\192.168.5.230\Pless_Data\490_Tech_Projects\440_Scripts\Pless_MON\$hostname
-Move-Item -Path C:\PlessMON_Agent\Reports\*.html -Destination \\192.168.5.230\Pless_Data\490_Tech_Projects\440_Scripts\Pless_MON\$hostname\
+$rep = Get-childitem -path "C:\PlessMON_Agent\Reports"
+If(test-path "\\192.168.5.230\Pless_Data\490_Tech_Projects\440_Scripts\Pless_MON\$hostname")
+{
+    foreach($file in $rep.Name)
+        {
+            If(test-path "\\192.168.5.230\Pless_Data\490_Tech_Projects\440_Scripts\Pless_MON\$hostname\$file")
+            {}
+            Else
+            {
+                Copy-Item -Path C:\PlessMON_Agent\Reports\$file -Destination \\192.168.5.230\Pless_Data\490_Tech_Projects\440_Scripts\Pless_MON\$hostname\$file
+            }
+        }
+}
+Else
+{
+    New-Item -ItemType Directory \\192.168.5.230\Pless_Data\490_Tech_Projects\440_Scripts\Pless_MON\$hostname
+    foreach($file in $rep.Name)
+    {
+        If(test-path "\\192.168.5.230\Pless_Data\490_Tech_Projects\440_Scripts\Pless_MON\$hostname\$file")
+        {}
+        Else
+        {
+            Copy-Item -Path C:\PlessMON_Agent\Reports\$file -Destination \\192.168.5.230\Pless_Data\490_Tech_Projects\440_Scripts\Pless_MON\$hostname\$file
+        }
+    }
+}
